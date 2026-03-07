90bbbf3e4250011e2c447a1da35c84b710707eeb
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-03-07 14:18:19 +0100

    :zap: correct redirect when logout

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 export function SignOutButton() {
   return (
     <button
-      onClick={() => signOut({ redirect: true, callbackUrl: "/login" })}
+      onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
       className="px-6 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white font-semibold transition-colors"
     >
       Sign Out
