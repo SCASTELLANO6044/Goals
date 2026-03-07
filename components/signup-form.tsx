cb74b31ca324529223aed9b6a926af2237498112
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-03-07 12:10:10 +0100

    :zap: Suppress hidratation warnings

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -33,7 +33,7 @@ export function SignupForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" suppressHydrationWarning={true}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Create your account</h1>
@@ -48,6 +48,7 @@ export function SignupForm({
                   type="email"
                   placeholder="m@example.com"
                   required
+                  suppressHydrationWarning={true}
                 />
                 <FieldDescription>
                   We&apos;ll use this to contact you. We will not share your
@@ -58,13 +59,13 @@ export function SignupForm({
                 <Field className="grid grid-cols-2 gap-4">
                   <Field>
                     <FieldLabel htmlFor="password">Password</FieldLabel>
-                    <Input id="password" type="password" required />
+                    <Input id="password" type="password" required suppressHydrationWarning={true} />
                   </Field>
                   <Field>
                     <FieldLabel htmlFor="confirm-password">
                       Confirm Password
                     </FieldLabel>
-                    <Input id="confirm-password" type="password" required />
+                    <Input id="confirm-password" type="password" required suppressHydrationWarning={true} />
                   </Field>
                 </Field>
                 <FieldDescription>
