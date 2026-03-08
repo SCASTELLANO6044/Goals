901f93edf13376e56636469654fdc8e41db3a50a
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-03-08 22:43:21 +0100

    :sparkles: Google oAuth with supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
 import { Hero47 } from "@/components/hero47";
+import { createClientForServer } from "@/lib/supabase/server";
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
-  return (
-    <div>
-      <Hero47 />
-    </div>
-  );
+
+export default async function Home() {
+  const supabase = createClientForServer();
+  const session = await (await supabase).auth.getUser();
+
+  if (!session.data.user){
+    return (
+      <div>
+        <Hero47 />
+      </div>
+    );
+  }
+
+  const {
+    data: {
+      user: { user_metadata, app_metadata },
+    },
+  } = session
+
+  const { name, email, user_name, avatar_url } = user_metadata
+
+  const userName = user_name ? `@${user_name}` : 'User Name Not Set'
+
+  console.log(session)
+
+  redirect('/profile/' + userName);
 }
